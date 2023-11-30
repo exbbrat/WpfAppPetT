f2600b8d0dc716b0cf55efcec81d48d29c01ad75
Lesta8335 <nastya@doublek.com.ua>
2023-11-30 19:28:53 +0200

    - Added ComboBoxes.Items = null verify; - Changed Timer code; - Added Start Timer button + code; - Added Reset Timer button + code.

--- SettingsWindow.xaml.cs
+++ SettingsWindow.xaml.cs
@@ -14,16 +14,13 @@ using System.Windows.Shapes;
 using System.Text.RegularExpressions;
 namespace WpfAppPetT
 {
-    /// <summary>
-    /// СДЕЛАТЬ ИМПОРТ ДАННЫХ ИЗ ЛИСТВЬЮ 1 ВО 2 В РАЗНЫХ ОКНАХ
-    /// </summary>
     public partial class SettingsWindow : Window
     {
         public SettingsWindow()
         {
+
             InitializeComponent();
         }
-
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9]+");
@@ -38,6 +35,7 @@ namespace WpfAppPetT
             {
                 // Получите содержимое выбранного элемента ComboBoxItem
                 string selectedItemContent1 = selectedComboBoxItem.Content.ToString();
+
 
                 // Добавьте содержимое в ListView
                 ListViewLeft.Items.Add(selectedItemContent1);
@@ -73,31 +71,81 @@ namespace WpfAppPetT
 
             if (showMessage)
             {
+
                 // Вывод MessageBox с двумя кнопками ("OK" и "Отмена")
                 MessageBoxResult result = MessageBox.Show("Данные были успешно добавлены в ListView. Продолжить?", "Подтверждение", MessageBoxButton.OKCancel);
 
                 if (result == MessageBoxResult.OK)
                 {
-                    // Пользователь нажал "OK", выполните необходимые действия
-                    this.Hide();
+                    if (result == MessageBoxResult.OK)
+                    {
+                        bool isAllSelected = ComboBoxLeft.SelectedItem != null && ComboBoxMiddle.SelectedItem != null && ComboBoxRight.SelectedItem != null;
 
-                    TestWindow window = new TestWindow(ListViewLeft.Items.OfType<string>().ToList());
-                    window.Show();
-
+                        if (isAllSelected)
+                        {
+                            this.Hide();
+                            TestWindow window = new TestWindow(ListViewLeft.Items.OfType<string>().ToList());
+                            window.Show();
+                            window.UpdateListViewVisibility();
+                            // Проверка на заполненность ComboBox перед переходом на другое окно
+                        }
+                        else
+                        {
+                            MessageBox.Show("Заполните все ComboBox");
+                        }
+                    }
                 }
                 else
                 {
+                    MessageBox.Show("cancel");
                     // Пользователь нажал "Отмена", выполните необходимые действия или ничего не делайте
                 }
             }
         }
+
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
             ListViewLeft.Items.Clear();
         }
 
+        //private void ComboBoxLeft_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    //this.Hide();
 
+        //    //TestWindow window = new TestWindow(ListViewLeft.Items.OfType<string>().ToList());
+        //    //window.Show();
+        //    //window.UpdateListViewVisibility();
+        //    System.Windows.Forms.MessageBox.Show("Test");
+        //    var tme = "";
+        //    if (ComboBoxLeft.Items.ToString() == tme)
+        //    {
+        //        MessageBox.Show("выберите что-нибудь");
+        //    }
+        //    else
+        //    {
+        //        //this.Hide();
+
+        //        //TestWindow window = new TestWindow(ListViewLeft.Items.OfType<string>().ToList());
+        //        //window.Show();
+        //        //window.UpdateListViewVisibility();
+        //    }
+        //}
+        //private void ComboBoxMiddle_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    System.Windows.Forms.MessageBox.Show("Test1");
+
+        //}
+
+        //private void ComboBoxRight_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    System.Windows.Forms.MessageBox.Show("Test2");
+
+        //}
+
+        // каждое событие selectionchanged будет отвечать за наличие пустого комбобокса и его считывание, при условии, что комбобокс пустой, может выводиться сообщение. Далее нужно объявить эти события в кнопке в условии нажатия OK в MessageBox.
+
+        //перегрузка методов и кастование - посмотреть)
 
         //private void ApplyButtonMiddle_Click(object sender, RoutedEventArgs e)
         //{
--- TestWindow.xaml.cs
+++ TestWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfAppPetT
 {
@@ -20,23 +21,46 @@ namespace WpfAppPetT
     /// </summary>
     public partial class TestWindow : Window
     {
-        public TestWindow(List<string> data)
+        private DispatcherTimer timer; // Об'єкт таймера
+        private TimeSpan timeLeft; // Залишений час
+        internal TestWindow(List<string> data)
         {
             InitializeComponent();
+            //StartTimer();
 
             foreach (var item in data)
             {
-                ListViewRight.Items.Add(item);
+                ListViewP1.Items.Add(item);
             }
         }
 
+        //private void StartTimer()
+        //{
+        //    timeLeft = TimeSpan.FromMinutes(3); // Встановлення початкового часу (3 хвилини)
+
+        //    // Створення та налаштування таймера
+        //    timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+        //    {
+        //        timerLabel.Content = timeLeft.ToString(@"mm\:ss"); // Оновлення Label з відліком часу
+
+        //        if (timeLeft == TimeSpan.Zero)
+        //        {
+        //            timer.Stop(); // Зупинка таймера, коли час вичерпаний
+        //            MessageBox.Show("Час вийшов!");
+        //        }
+
+        //        timeLeft = timeLeft.Add(TimeSpan.FromSeconds(-1)); // Віднімання однієї секунди
+        //    }, Application.Current.Dispatcher);
+
+        //    timer.Start(); // Початок таймера
+        //}
         private void ImportDataButton_Click(object sender, RoutedEventArgs e)
         {
             // Очищаем существующие элементы в ListView перед импортом
-            ListViewRight.Items.Clear();
+            ListViewP1.Items.Clear();
 
             // Имя файла для импорта
-            string filePath = @"C:\Users\nasty\source\repos\WpfAppPetT\imported_data.txt";
+            string filePath = @"D:\____Файли з диску\C# Python\Программирование\GitHub_Projects\imported_data.txt";
 
             try
             {
@@ -47,7 +71,7 @@ namespace WpfAppPetT
                     string[] lines = File.ReadAllLines(filePath);
                     foreach (string line in lines)
                     {
-                        ListViewRight.Items.Add(line);
+                        ListViewP1.Items.Add(line);
                     }
 
                     MessageBox.Show($"Данные успешно импортированы из файла: {filePath}", "Импорт завершен", MessageBoxButton.OK);
@@ -62,11 +86,10 @@ namespace WpfAppPetT
                 MessageBox.Show($"Произошла ошибка при чтении файла: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
-
         private void ExportDataButton_Click(object sender, RoutedEventArgs e)
         {
             // Проверяем, есть ли данные в ListView
-            if (ListViewRight.Items.Count > 0)
+            if (ListViewP1.Items.Count > 0)
             {
                 // Создаем или перезаписываем текстовый файл
                 string filePath = "exported_data.txt";
@@ -74,7 +97,7 @@ namespace WpfAppPetT
                 using (StreamWriter writer = new StreamWriter(filePath))
                 {
                     // Записываем данные из ListView в файл
-                    foreach (var item in ListViewRight.Items)
+                    foreach (var item in ListViewP1.Items)
                     {
                         writer.WriteLine(item.ToString());
                     }
@@ -86,6 +109,92 @@ namespace WpfAppPetT
             {
                 MessageBox.Show("Нет данных для экспорта.", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
+        }
+
+        internal void UpdateListViewVisibility()
+        {
+            // Настраиваем видимость ListView в зависимости от значения numberOfListViewsToShow
+            //ListViewP1.Visibility = numberOfListViewsToShow >= 1 ? Visibility.Visible : Visibility.Collapsed;
+            //ListViewP2.Visibility = numberOfListViewsToShow >= 2 ? Visibility.Visible : Visibility.Collapsed;
+            //ListViewP3.Visibility = numberOfListViewsToShow >= 3 ? Visibility.Visible : Visibility.Collapsed;
+            //ListViewP4.Visibility = numberOfListViewsToShow >= 4 ? Visibility.Visible : Visibility.Collapsed;
+            //ListViewP5.Visibility = numberOfListViewsToShow >= 5 ? Visibility.Visible : Visibility.Collapsed;
+            //ListViewP6.Visibility = numberOfListViewsToShow >= 6 ? Visibility.Visible : Visibility.Collapsed;
+            //// ... настраиваем остальные ListView до ListViewP6 ...
+        }
+
+        private void TestWindow_loaded()
+        {
+            //string selectedValue = propGamers;
+
+            //var temp = int.Parse(selectedValue);
+            //var tmpint = 0;
+
+            //// Определение количества ListViewToShow в зависимости от выбранного элемента в ComboBox
+            //if (temp == 2)
+            //{
+            //    // show 1, 2 player
+            //    ListViewP1.Visibility = Visibility.Visible;
+            //    ListViewP2.Visibility = Visibility.Visible;
+            //}
+            //else if (temp == 4)
+            //{
+            //    // show 4 play
+            //    numberOfListViewsToShow = 4;
+            //}
+            //else if (temp == 6)
+            //{
+            //    // show 6 play 
+            //    numberOfListViewsToShow = 6;
+            //}
+        }
+        private bool isTimerRunning = false; // Флаг для отслеживания состояния таймера
+
+        private void StartTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isTimerRunning)
+            {
+                timeLeft = TimeSpan.FromMinutes(3); // Установка начального времени (3 минуты)
+
+                // Создание и настройка таймера
+                timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+                {
+                    timerLabel.Content = timeLeft.ToString(@"mm\:ss"); // Обновление Label с отсчетом времени
+
+                    if (timeLeft == TimeSpan.Zero)
+                    {
+                        timer.Stop(); // Остановка таймера, если время вышло
+                        MessageBox.Show("Время вышло!");
+                        isTimerRunning = false; // Сброс флага
+                    }
+
+                    timeLeft = timeLeft.Add(TimeSpan.FromSeconds(-1)); // Уменьшение времени на одну секунду
+                }, Application.Current.Dispatcher);
+
+                timer.Start(); // Запуск таймера
+                isTimerRunning = true; // Установка флага
+            }
+            else
+            {
+                MessageBox.Show("Таймер уже запущен!"); // Сообщение, если таймер уже запущен
+            }
+        }
+
+        private void ResetTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Остановка таймера, если он запущен
+            if (timer != null && timer.IsEnabled)
+            {
+                timer.Stop();
+            }
+
+            // Установка времени на начальное значение (3 минуты в данном случае)
+            timeLeft = TimeSpan.FromMinutes(3);
+
+            // Обновление Label с отображением времени на начальное значение
+            timerLabel.Content = timeLeft.ToString(@"mm\:ss");
+
+            isTimerRunning = false; // Сброс флага запущенности таймера 
         }
     }
 }
